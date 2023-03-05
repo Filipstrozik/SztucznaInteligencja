42866e4d352baef52b46cf64ebd7661092b9b0a1
Filipstrozik <filipshelby@gmail.com>
2023-03-05 16:48:16 +0100

    initial djikstra

---
+++ Z1/Z1/Z1/Dijkstra.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1
+{
+    public class DijkstraAlgorithm
+    {
+        private static int GetMinDistance(Dictionary<int, int> distances, HashSet<int> unvisited)
+        {
+            int minDistance = int.MaxValue;
+            int minNode = 0;
+
+            foreach (var pair in distances)
+            {
+                int node = pair.Key;
+                int distance = pair.Value;
+
+                if (unvisited.Contains(node) && distance < minDistance)
+                {
+                    minDistance = distance;
+                    minNode = node;
+                }
+            }
+
+            return minNode;
+        }
+
+        public static Dictionary<int, int> FindShortestPaths(Dictionary<int, Dictionary<int, int>> graph, int startNode)
+        {
+            Dictionary<int, int> distances = new Dictionary<int, int>();
+            HashSet<int> unvisited = new HashSet<int>();
+
+            foreach (var node in graph.Keys)
+            {
+                distances[node] = int.MaxValue;
+                unvisited.Add(node);
+            }
+
+            distances[startNode] = 0;
+
+            while (unvisited.Count > 0)
+            {
+                int currentNode = GetMinDistance(distances, unvisited);
+                unvisited.Remove(currentNode);
+
+                if (graph.ContainsKey(currentNode))
+                {
+                    foreach (var neighbor in graph[currentNode])
+                    {
+                        int distanceToNeighbor = neighbor.Value;
+                        int distanceFromStart = distances[currentNode] + distanceToNeighbor;
+
+                        if (distanceFromStart < distances[neighbor.Key])
+                        {
+                            distances[neighbor.Key] = distanceFromStart;
+                        }
+                    }
+                }
+            }
+
+            return distances;
+        }
+    }
+}
---
+++ Z1/Z1/Z1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Z1;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<int, Dictionary<int, int>> graph = new Dictionary<int, Dictionary<int, int>>();
+
+        // Add nodes and edges to the graph
+        graph[1] = new Dictionary<int, int>();
+        graph[1][2] = 1;
+        graph[1][3] = 4;
+
+        graph[2] = new Dictionary<int, int>();
+        graph[2][4] = 6;
+
+        graph[3] = new Dictionary<int, int>();
+        graph[3][4] = 2;
+        graph[3][5] = 5;
+
+        graph[4] = new Dictionary<int, int>();
+        graph[4][5] = 3;
+
+        graph[5] = new Dictionary<int, int>();
+
+        int startNode = 1;
+        Dictionary<int, int> shortestDistances = DijkstraAlgorithm.FindShortestPaths(graph, startNode);
+
+        Console.WriteLine("Shortest distances from node {0}:", startNode);
+
+        foreach (var pair in shortestDistances)
+        {
+            Console.WriteLine("Node {0}: {1}", pair.Key, pair.Value);
+        }
+    }
+}
